1439bb76f0707b40003e63427518f532ce423267
Iamneo <techadmin@iamneo.ai>
2023-11-17 12:09:04 +0000

    Backup Commit

--- dotnetapp/Controllers/AdminController.cs
+++ dotnetapp/Controllers/AdminController.cs
@@ -20,15 +20,30 @@ namespace dotnetapp.Controllers
             _context = context;
         }
 
+        [HttpGet]
+        [Route("GetPlayers")]
         public IActionResult GetPlayers(){
+            var plist = _context.Players.ToList();
+            return Ok(plist);
+        }
+
+        [HttpPost]
+        [Route("PostPalyers")]
+        public IActionResult PostPlayers(Player player){
+            _context.Players.Add(player);
+            _context.SaveChanges();
             return Ok();
         }
 
-        public IActionResult GetTeams(){
-            return Ok();
-        }
-
-        public IActionResult PutPlayer(int id){
+        [Route("PutPlayer")]
+        [HttpPut]
+        public IActionResult PutPlayer(int id, Player player){
+            var pl = _context.Players.Find(id);
+            if(pl!=null){
+                pl.Age=player.Age;
+                pl.Name=player.Name;
+                plock.T
+            }
             return Ok();
         }
 
