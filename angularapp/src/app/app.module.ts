f5c5c1c4908e4f421bbe3419f526a12f1c8b97ab
Iamneo <techadmin@iamneo.ai>
2023-11-23 05:39:03 +0000

    Backup Commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -4,14 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { TeamComponent } from './team/team.component';
+//import { TeamComponent } from './team/team.component';
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { DeleteComponent } from './delete/delete.component';
 import { FindplayerComponent } from './findplayer/findplayer.component';
 
-import { RegisterComponent } from './services/register/register.component';
+//import { RegisterComponent } from './services/register/register.component';
 import { GetdetailComponent } from './getdetail/getdetail.component';
 import { AddInfoComponent } from './add-info/add-info.component';
 
@@ -20,12 +20,12 @@ import { AddInfoComponent } from './add-info/add-info.component';
   declarations: [
     AppComponent,
   
-    TeamComponent,
+    //TeamComponent,
     LoginComponent,
     DeleteComponent,
     FindplayerComponent,
    
-    RegisterComponent,
+    //RegisterComponent,
     GetdetailComponent,
     AddInfoComponent,
     
